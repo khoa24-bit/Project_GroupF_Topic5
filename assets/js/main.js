3a17a9b613a6b23a29434517bd50397bdf648629
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-09-28 12:36:50 +0700

    Add button back to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Thu nho header khi cuon
 window.addEventListener('scroll', function() {
     var header = document.querySelector('.header');
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -8,3 +9,24 @@ window.addEventListener('scroll', function() {
         header.classList.remove('shrink');
     }
 });
+
+// Lang nghe su kien cuon trang
+window.addEventListener('scroll', function() {
+    var backToTopButton = document.getElementById('backToTop');
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > 200) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
+
+// Hieu ung muot ma
+document.getElementById('backToTop').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
